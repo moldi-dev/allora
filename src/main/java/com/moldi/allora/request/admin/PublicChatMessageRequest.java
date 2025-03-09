5bf163d998d6516c2638ce0ed945106aa34c58ba
moldi-dev <sumalamadingdong@gmail.com>
2025-03-09 18:05:40 +0200

    Fixed the max length of the public chat message

--- src/main/java/com/moldi/allora/request/admin/PublicChatMessageRequest.java
+++ src/main/java/com/moldi/allora/request/admin/PublicChatMessageRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record PublicChatMessageRequest(
         @NotEmpty(message = "The message is required")
-        @Size(max = 500, message = "The message must contain at most 500 characters")
+        @Size(max = 200, message = "The message must contain at most 200 characters")
         String content
 ) {
 
